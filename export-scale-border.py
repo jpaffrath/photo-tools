7ce771272330eb48a66368b13305b918cf25fe15
Julius Paffrath <github@julius-paffrath.de>
2025-03-02 17:07:10 +0100

    Create export-scale-border.py

---
+++ export-scale-border.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageOps
+import glob, os
+
+def scale(input, output):
+    img = Image.open(input)
+
+    new_size = (int(img.width * 0.5), int(img.height * 0.5))
+    img_resized = img.resize(new_size, Image.Resampling.LANCZOS)
+
+    img_resized_border = ImageOps.expand(img_resized, border=75, fill='white')
+    img_resized_border.save(output)
+
+
+wildcard = '*.JPG'
+folder = 'imgs'
+paths = glob.glob(os.path.join(folder, wildcard))
+
+for path in paths:
+    print('Processing ' + path)
+    scale(path, path + 'out.jpeg')
